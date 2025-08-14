5731fb2f597bb4945c3a4dcdc3bce3c15ff3da74
Yevhen Kliuchyk <kliuchyk@gmail.com>
2025-08-14 15:38:36 +0200

    build new issue page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <div>Hello World!</div>
-      <Button>TODO</Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 }
