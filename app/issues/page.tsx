f37d21d756313de414fc27f40785f1861edd5db6
Yevhen Kliuchyk <kliuchyk@gmail.com>
2025-08-15 13:40:13 +0200

    show issues in table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+import prisma from "@/prisma/client";
+import { Box, Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
 
-export default function IssuesPage() {
-  return <div>IssuesPage</div>;
+export default async function IssuesPage() {
+  const issues = await prisma.issue.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <div>
+      <Box className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </Box>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map(({ id, status, title, createdAt }) => (
+            <Table.Row key={id}>
+              <Table.Cell>
+                {title}
+                <div className="block md:hidden">{status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">{status}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <div>
-      <div>Hello World!</div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
-    </div>
-  );
+  return <div>Hello World!</div>;
 }
